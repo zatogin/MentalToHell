7be18714aa43bb022bbd7cc5b28c423a8a53a293
Олександр Затогін <zatogin1995@gmail.com>
2019-08-27 18:01:07 +0300

    Trying to do new design

--- MentalToHell/wwwroot/js/site.js
+++ MentalToHell/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$("#menu-open").click(function () {
+    $("#main-menu-hidden").removeClass("hidden");
+});
+
+$("#menu-close").click(function () {
+    $("#main-menu-hidden").addClass("hidden");
+});
